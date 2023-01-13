d5836539d3c08f92640de2a82db5dad8dac154af
sjknfjk <119666452+sjknfjk@users.noreply.github.com>
2023-01-13 13:11:32 +0800

    Add files via upload

--- Third Round.sql
+++ Third Round.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE student-STATUS;
-CREATE TABLE student;
-CREATE TABLE class;
+CREATE TABLE `student-status`.`student` ( `name` VARCHAR(10), `id` INT, `age` INT, `sex` VARCHAR(1) DEFAULT '男' ) ENGINE=INNODB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO student(id,NAME,age)
+VALUES  (832203220,"林慕宸",19)
+	(832203221,"林浩哲",18)
+	(832203222,"周乐轩",18);
+CREATE TABLE `student-status`.`class` ( `class` VARCHAR(10), `name` VARCHAR(10), `id` INT, `age` INT, `sex` VARCHAR(1) DEFAULT '男', `time` DATE ) ENGINE=INNODB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 INSERT INTO class(NAME,id,age,sex)
 SELECT NAME,id,age,sex FROM `student`;
 UPDATE class SET class='CS161FZ',TIME='2023-01-17' WHERE NAME='林慕宸';
